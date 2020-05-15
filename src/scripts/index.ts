e8a776134e699346ff4668103250c70ad2ecf8f2
Victor Nogueira <felladrin@gmail.com>
2020-05-15 14:17:58 -0400

    Avoid injecting (instantiating) classes that are already injected in GameMenu and GameListeners
    This fixes the problem of the sound icon on top bar not working.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,17 +1,7 @@
 import "../styles/index.scss";
-import { GameCountDownTimer } from "./class/GameCountDownTimer";
 import { GameListeners } from "./class/GameListeners";
 import { GameMenu } from "./class/GameMenu";
-import { GamePlayScene } from "./class/GamePlayScene";
-import { GameSceneManager } from "./class/GameSceneManager";
-import { GameStreakManager } from "./class/GameStreakManager";
-import { GameTopBar } from "./class/GameTopBar";
 import { injector } from "./const/injector";
 
-injector.injectClass(GameTopBar);
-injector.injectClass(GameStreakManager);
 injector.injectClass(GameMenu);
-injector.injectClass(GameSceneManager);
-injector.injectClass(GamePlayScene);
-injector.injectClass(GameCountDownTimer);
 injector.injectClass(GameListeners);
